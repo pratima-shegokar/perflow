fa0d599f6a3459ac9e63aa6c9e079cc8ea04f443
Pratima Shegokar <pratphad@gmail.com>
2019-11-27 19:53:38 +0100

    Initial commit

---
+++ src/test/java/se/kth/sda6/perflow/PerflowApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.sda6.perflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
