edf410a29fe5ec6079e8c84b63aa91363a0c5afd
Melany <melanycristyg@gmail.com>
2019-12-01 15:56:52 +0100

    I change some misspelled in the code and comments

--- frontend/src/components/MyProject.js
+++ frontend/src/components/MyProject.js
@@ -3,11 +3,11 @@ import React from 'react';
 class MyProjects extends React.Component {
     render() {
         return (
-            // /Projects should be the page where you can see the project specifically
+            // /ProjectForm should be the page where you can see the project specifically
             <React.Fragment>
                 <div className="text-center mt-5">
                     <p className="text-muted">
-                        You don't have any projects yet. Would you like to <a href="/Project" className="text-reset">create a new one?</a>.</p>
+                        You don't have any projects yet. Would you like to <a href="/ProjectForm" className="text-reset">create a new one?</a>.</p>
                 </div>
             </React.Fragment>
         )
