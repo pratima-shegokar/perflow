f535b0b57b5c3e64113321bbc451af97a1647db1
Abdul Hameed <abdnaqip1980@gmail.com>
2019-12-02 18:34:38 +0100

    Added Record Service with normal functionality of adding, updating, getting and deleting records, and added to it one more method getByProject which is reflected also in RecordRepo by the way.

---
+++ src/main/java/se/kth/sda6/perflow/records/RecordService.java
@@ -0,0 +1,49 @@
+package se.kth.sda6.perflow.records;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.kth.sda6.perflow.projects.Project;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+class RecordService {
+
+    @Autowired
+    private RecordRepo recordRepo;
+
+    List<Record> getAll(){
+        return recordRepo.findAll();
+    }
+
+    Optional<Record> getById(long id){
+        return recordRepo.findById(id);
+    }
+
+    //Warning: This must be tested
+    //get all records belong to a certain project, and sorted by interval
+    List<Record> getByProject(Project project){
+        return recordRepo.findByProject(project).stream().sorted(new Comparator<Record>() {
+            @Override
+            public int compare(Record r1, Record r2) {
+                return r1.getInterval()-r2.getInterval();
+            }
+        }).collect(Collectors.toList());
+    }
+
+    Record add(Record record){
+        return recordRepo.save(record);
+    }
+
+    Record update(Record record){
+        return recordRepo.save(record);
+    }
+
+    void deleteById(long id){
+        recordRepo.deleteById(id);
+    }
+
+}
