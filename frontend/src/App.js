d010f491fe3269bd6dd1f6f3ed492e8a063b6a49
rsidhi088 <rsidhi088@gmail.com>
2019-12-03 13:46:19 +0100

    Added the initial commit with tables for project details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Table1 from './components/Table1'
+import Navigation from './components/Navigation'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import MyProjects from './components/MyProject';
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Router>
-        <Route path="/" component={App}>
-          <Route path="/MyProjects" component={MyProjects} />
-        </Route>
-      </Router>
-    </React.Fragment>
-  );
+ 
+var data = [
+  { name: 'Project Name', value: 'Swiss Tower'},
+  { name: 'Duration', value: '24 months'},
+  { name: 'Budget', value: '40000'}
+];
+ 
+ 
+class App extends Component {
+  render() {
+    return (
+      <div>
+       
+      
+      <div className="App">
+        <p className="Table-header">Project Specific  Details</p>
+        <Table1 data={data}/>
+        
+        
+        </div> 
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+//This whole file (or component) should only be touch if the team decides to change it.
+//This component it's already set up for appear in every page.
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                    <a class="navbar-brand" href="/Homepage">Home</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/UpdatePage">Update<span class="sr-only">(current)</span></a>
+                            </li>   
+                        </ul>   
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navigation;
