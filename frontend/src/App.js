fec0954576ec07e4a133f5460e9e0367625245e3
jshortz <jessica.shortz@gmail.com>
2019-12-03 14:53:01 +0100

    Added a very minimum Why Perflow page. Added link to navbar. Moved MyProject.js into appropriate folder for code structure. Edited comments in Navbar.js for minor grammar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import MyProjects from './components/MyProject';
+import MyProjects from './components/ProjectsList/MyProject';
+import WhyPerflow from './components/WhyPerflow/WhyPerflow';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Route path="/" component={App}>
           <Route path="/MyProjects" component={MyProjects} />
+          <Route path="/WhyPerflow" component={WhyPerflow} />
         </Route>
       </Router>
     </React.Fragment>
---
+++ frontend/src/components/WhyPerflow/WhyPerflow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class WhyPerflow extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="text-center mt-5">
+                    <h1>Why Choose Perflow?</h1>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default WhyPerflow;
