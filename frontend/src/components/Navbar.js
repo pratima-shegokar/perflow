268ae3b7f9d3697f2a483b4cba06b726a631c414
Pratima Shegokar <pratphad@gmail.com>
2019-12-05 00:33:45 +0100

    remove sign up link from navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,9 +23,6 @@ class Navbar extends React.Component {
                             <li class="nav-item">
                                 <a class="nav-link" href="/LoginPage">Login</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/RegistrationPage">Sign Up</a>
-                            </li>
                         </ul>
                         <form class="form-inline my-2 my-lg-0">
                             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
