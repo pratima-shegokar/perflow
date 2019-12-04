710b34bdd07d47fdb57ec68c1efc12f37cb055e2
Pratima Shegokar <pratphad@gmail.com>
2019-12-05 00:10:22 +0100

    Added rendering of registration form

---
+++ frontend/src/components/Login/RegistrationPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Auth from "../../services/Auth";
+import RegistrationForm from "./RegistrationForm";
+
+class RegistrationPage extends React.Component {
+    async register(registrationData) {
+        const registerSuccess = await Auth.register(registrationData);
+        if (!registerSuccess) {
+            alert("Couldn't register check credentials and try again");
+        }
+        else {
+            alert("User created successfully. Please login with new user.");
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="wrapper">
+                    <div className="container ">
+                        <div className="row justify-content-center" id="loginLogoDiv">
+                            <img alt="Logo" src={('./PerflowLogo.png')} width={150} height={150} id="registerationLogo"/>
+                        </div>
+                        <div className="row justify-content-center mt-0">
+                            <div className="col-md-6 strong-shadow">
+                                <RegistrationForm onSubmit={this.register} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+export default RegistrationPage;
