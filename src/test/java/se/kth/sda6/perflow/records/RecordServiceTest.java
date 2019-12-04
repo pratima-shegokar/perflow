295db7d7a30e8818c2f2734559377d85cac2578a
Abdul Hameed <abdnaqip1980@gmail.com>
2019-12-04 10:56:58 +0100

    Added a test of the basic functionality of calcCPCIFList method in RecordService class.

--- src/test/java/se/kth/sda6/perflow/records/RecordServiceTest.java
+++ src/test/java/se/kth/sda6/perflow/records/RecordServiceTest.java
@@ -50,4 +50,43 @@ class RecordServiceTest {
     }
 
 
+    @Test
+    void calcCPCIFListTest(){
+        //define a project obj
+        Project project = new Project("P111","Swiss Tower",12,150000000
+                ,6,50000,50000,30000000,0.05,2,0.08);
+
+        //use the calcCPVList method to provide the cum PV List
+        List<Double> cPvList;
+        cPvList = recordService.calcCPVList(project);
+
+        //use the calcCPCIFList method to provide the cum CPCIF List
+        List<Double> cPCIFList;
+        cPCIFList = recordService.calcCPCIFList(cPvList, project);
+
+        //The cum planned cash inflow are estimated on excel and filled here manually in a compare list
+        List<Double> compareList = new ArrayList<>();
+        compareList.add(30000000d);
+        compareList.add(30000000d);
+        compareList.add(30037513d);
+        compareList.add(31584620d);
+        compareList.add(36905711d);
+        compareList.add(47132984d);
+        compareList.add(62266438d);
+        compareList.add(81173877d);
+        compareList.add(101590910d);
+        compareList.add(119888472d);
+        compareList.add(133131914d);
+        compareList.add(140343018d);
+        compareList.add(142500000d);
+        compareList.add(150000000d);
+
+        //all items in the list should be approximately equivalent with a margin less than 1
+        boolean approxEquality = (Math.abs((cPCIFList.get(0) - compareList.get(0))) < 1);
+        for (int i = 1; i < cPCIFList.size(); i++){
+            approxEquality = approxEquality && (Math.abs((cPCIFList.get(i) - compareList.get(i))) < 1);
+        }
+
+        assertTrue(approxEquality);
+    }
 }
