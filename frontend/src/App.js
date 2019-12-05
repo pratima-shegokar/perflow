71651437004f89d3450b0e150d7cb925c00b1cc9
MiladYaqo <milad2.yaqo@yahoo.com>
2019-12-05 12:38:32 +0100

    resolved the merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './components/Home/Home'
 import MyProjects from './components/ProjectsList/MyProject';
 import WhyPerflow from './components/WhyPerflow/WhyPerflow';
 import Navbar from './components/Navbar';
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Router>
         <Route path="/" component={App}>
+          <Route path="/Homepage" component={Home} />
           <Route path="/MyProjects" component={MyProjects} />
           <Route path="/WhyPerflow" component={WhyPerflow} />
         </Route>
