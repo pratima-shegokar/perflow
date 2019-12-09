14dd68da4c9f1165e99d4a963ac5f777aac3b2e4
Abdul Hameed <abdnaqip1980@gmail.com>
2019-12-09 17:13:15 +0100

    no change

--- frontend/src/components/Projects/MyProject.js
+++ frontend/src/components/Projects/MyProject.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
+import ProjectsApi from "./../../api/ProjectsApi";
 
 class MyProjects extends React.Component {
-    render() {
-        return (
-            // /ProjectForm should be the page where you can create a new project.
-            <React.Fragment>
-                <div className="text-center mt-5">
-                    <p className="text-muted">
-                        You don't have any projects yet. Would you like to <a href="/ProjectForm" className="text-reset">create a new one?</a></p>
-                </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      // /ProjectForm should be the page where you can create a new project.
+      <React.Fragment>
+        <div className="text-center mt-5">
+          <p className="text-muted">
+            You don't have any projects yet. Would you like to{" "}
+            <a href="/ProjectForm" className="text-reset">
+              create a new one?
+            </a>
+          </p>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default MyProjects;
