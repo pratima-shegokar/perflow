ed9baade334a24b1ba79bf87d835a2c2771bc6ce
jshortz <jessica.shortz@gmail.com>
2019-12-11 13:29:38 +0100

    added componentdidmount to change bg color, changed width in Slider.css to make bigger

--- frontend/src/components/landing/LandingPage.js
+++ frontend/src/components/landing/LandingPage.js
@@ -8,9 +8,13 @@ export default class LandingPage extends Component {
     this.state = {}
   }
 
+  componentDidMount() {
+      document.body.style = 'background: #986AD9; color: white;';
+  }
+
   render() {
     return (
-      <div>
+      <div className="mt-3">
         <Slider />
       </div>
     );
