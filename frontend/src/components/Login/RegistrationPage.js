625225f9d852dfd6b21efd9483fb213524552731
jshortz <jessica.shortz@gmail.com>
2019-12-12 10:40:03 +0100

    Update RegistrationPage.js
    Updated RegistrationPage background

--- frontend/src/components/Login/RegistrationPage.js
+++ frontend/src/components/Login/RegistrationPage.js
@@ -15,7 +15,7 @@ class RegistrationPage extends React.Component {
     }
 
     componentDidMount() {
-        document.body.style = 'background: #986AD9; color: white;';
+        document.body.style = 'background-image: linear-gradient(to bottom right, #177b91, #63D8F2, #177b91); color: white;';
     }
 
     render() {
