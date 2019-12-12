ec3647eae0d72034510f1ccaf965d54d519a7965
rsidhi088 <rsidhi088@gmail.com>
2019-12-12 10:27:47 +0100

    Fixed the labels and formatting

--- frontend/src/components/Projects/ProjPanel.js
+++ frontend/src/components/Projects/ProjPanel.js
@@ -1,107 +1,192 @@
 
-import { Table } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-import React from "react";
 
-function ProjPanel({ onSubmit }) {
-  console.log(onSubmit)
-  const [projectUniqueNumber, getProjectUniqueNumber] = React.useState("");
-  const [projectName, getProjectName] = React.useState("");
-  const [duration, getDuration] = React.useState("");
-  const [budget, getBudget] = React.useState("");
-  const [peakInterval, getPeakInterval] = React.useState("");
-  const [firstPV, getFirstPV] = React.useState("");
-  const [lastPV, getLastPV] = React.useState("");
-  const [discountRate, getDiscountRate] = React.useState("");
+export class ProjPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { proj: {} };
+  }
+  componentDidMount() {
+    const {
+      projectUniqueNumber,
+      projectName,
+      duration,
+      budget,
+      peakInterval,
+      firstPV,
+      lastPV,
+      advPayment,
+      perfBond,
+      creditTime,
+      discountRate
+    } = this.props.location.state.project;
 
-  const handleSubmit = () => {
-    // Invoke the passed in event callback
-    onSubmit({
-      projectUniqueNumber: projectUniqueNumber,
-      projectName: projectName,
-      duration: duration,
-      budget: budget,
-      peakInterval: peakInterval,
-      firstPV: firstPV,
-      lastPV: lastPV,
-      discountRate: discountRate
+    this.setState({
+      projectUniqueNumber,
+      projectName,
+      duration,
+      budget,
+      peakInterval,
+      firstPV,
+      lastPV,
+      advPayment,
+      perfBond,
+      creditTime,
+      discountRate
     });
   }
+  render() {
+    const { project } = this.props.location.state;
 
-  return (
-    <div className="App">
-
-      <p className="Table-header">Project Specific  Details</p>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-8">
-            <div class="card-body">
-              <Table className="mt-2" striped bordered hover size="sm">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Value</th>
-                  </tr>
-                </thead>
-                <tbody>                  
-                    <tr key={projectUniqueNumber}>
-                      <td>ProjectName</td>
-                      <td>{getProjectName}</td>
+    return (
+      <div className="App">
+        <p>{this.props.location.state.proj}</p>
+        <p
+          className="Table-header"
+          style={{ color: "black", fontSize: 20, fontWeight: 4 }}
+        >Project Specific Details
+        </p>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="card-body">
+                <Table className="mt-2" striped bordered hover size="sm">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Value</th>
                     </tr>
-                    <tr >
-                      <td>Duration</td>
-                      <td>{getDuration}</td>
-                    </tr>
-                    <tr >
-                      <td>Budget</td>
-                      <td>{getBudget}</td>
-                    </tr>
-                    <tr >
-                      <td>PeakInterval</td>
-                      <td>{getPeakInterval}</td>
-                    </tr>
-                    <tr >
-                      <td>FirstPV</td>
-                      <td>{getFirstPV}</td>
-                    </tr>
-                    <tr >
-                      <td>LastPV</td>
-                      <td>{getLastPV}</td>
-                    </tr>
-                    <tr >
-                      <td>Discount</td>
-                      <td>{getDiscountRate}</td>
-                    </tr>
+                  </thead>
+                  <tbody>                    
+                        <tr >
+                          <td>Project Id</td>
+                          <td>{this.state.projectUniqueNumber}</td>
+                        </tr>                      
                     
-                      
-
-                </tbody>
-
-              </Table>
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Project Name</td>
+                          <td>{this.state.projectName}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Project Duration</td>
+                          <td>{this.state.duration}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Budget</td>
+                          <td>{this.state.budget}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Peak Interval</td>
+                          <td>{this.state.peakInterval}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>First PV</td>
+                          <td>{this.state.firstPV}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Last PV</td>
+                          <td>{this.state.lastPV}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Advanced Payment</td>
+                          <td>{this.state.advPayment}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>PerfBond</td>
+                          <td>{this.state.perfBond}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Credit Time</td>
+                          <td>{this.state.creditTime}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                  <tbody>                    
+                        <tr >
+                          <td>Discount Rate</td>
+                          <td>{this.state.discountRate}</td>
+                        </tr>                      
+                    
+                  </tbody>
+                </Table>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+                        
+                            
 
-      <div class="wrapper">
-        <a href="/EditProject" className="btn btn-outline-primary btn-sm ml-4" role="button">Edit</a>
-
-        <br /> <br />
-      </div>
-      <div class="graph-button">
-
-        <button type="button-gr" class="btn btn-outline-primary" >Periodical S-Curve Graph</button> &nbsp;&nbsp;&nbsp;
-             <button type="button-gr" class="btn btn-outline-primary" >Periodical Cash Flow Graph</button> &nbsp;&nbsp;&nbsp;
-             <button type="button-gr" class="btn btn-outline-primary" >Cumulative S-Curve Graph</button> &nbsp;&nbsp;&nbsp;
-             <button type="button-gr" class="btn btn-outline-primary">Cumulative Cash Flow Graph</button> &nbsp;&nbsp;&nbsp;
-             <button type="button-gr" class="btn btn-outline-primary" >EVM Board</button> &nbsp;&nbsp;&nbsp;
-             <button type="button-gr" class="btn btn-outline-primary">CFM Board</button>
-
-
-
-      </div>
-    </div>
-  )
-
-}
+          <div class="wrapper">
+            <Link
+              to={{
+                pathname: "/EditProject", state: {
+                  project: project
+                }
+              }}
+              className="btn btn-outline-primary btn-sm ml-4"
+              role="button"
+            >
+              Edit
+          </Link>
+            <br /> <br />
+          </div>
+          <div class="graph-button mr-auto ml-auto">
+            <a href="/SCurve" type="button-gr" class="btn btn-outline-primary">
+              Periodical S-Curve Graph
+          </a>{" "}
+            &nbsp;&nbsp;&nbsp;
+          <button type="button-gr" class="btn btn-outline-primary">
+              Periodical Cash Flow Graph
+          </button>
+            &nbsp;&nbsp;&nbsp;
+          <button type="button-gr" class="btn btn-outline-primary">
+              Cumulative S-Curve Graph
+          </button>
+            &nbsp;&nbsp;&nbsp;
+          <button type="button-gr" class="btn btn-outline-primary">
+              Cumulative Cash Flow Graph
+          </button>
+            &nbsp;&nbsp;&nbsp;
+          <button type="button-gr" class="btn btn-outline-primary">
+              EVM Board
+          </button>
+            &nbsp;&nbsp;&nbsp;
+          <button type="button-gr" class="btn btn-outline-primary">
+              CFM Board
+          </button>
+          </div>
+        </div>
+        );
+      }
+    }
+    
 export default ProjPanel;
