cadc5c8d24eb52ab386ba61d11b603306b5f8d11
Abdul Hameed <abdnaqip1980@gmail.com>
2019-12-12 11:38:11 +0100

    add getEvList and getAcList method with implementation.

--- frontend/src/api/RecordsApi.js
+++ frontend/src/api/RecordsApi.js
@@ -4,6 +4,14 @@ class RecordsApi {
   getPvList(projectId) {
     return Api.get("/records/pvs/" + projectId);
   }
+
+  getEvList(projectId) {
+    return Api.get("/records/evs/" + projectId);
+  }
+
+  getAcList(projectId) {
+    return Api.get("/records/acs/" + projectId);
+  }
 }
 
 export default new RecordsApi();
