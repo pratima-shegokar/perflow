21e8d4933ef84858e4428850955d7d02ed969ba0
Melany <melanycristyg@gmail.com>
2019-12-13 13:11:28 +0100

    Fixing footer and add picturesmissing in about us page

--- frontend/src/components/about/AboutUs.js
+++ frontend/src/components/about/AboutUs.js
@@ -6,12 +6,12 @@ class AboutUs extends React.Component {
         return (
             <React.Fragment>
                 <div className="card">
-                    <div className="heading">About Us</div>
+                    <div className="heading text-center">About Us</div>
                     <div className="row justify-content-center">
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
                                 <div class="card-body text-center">
-                                    <a href="https://www.linkedin.com/in/jessica-shortz-02572831/" target="_blank" style={{ textDecoration: 'none' }}>
+                                    <a href="https://www.linkedin.com/in/jessica-shortz-02572831/" target="blank" style={{ textDecoration: 'none' }}>
                                         <img className="rounded-circle" src="./aboutUs-Jessica.jpg" alt="..." />
                                     </a>
                                 </div>
@@ -78,7 +78,7 @@ class AboutUs extends React.Component {
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
                                 <div class="card-body text-center">
-                                    <img className="rounded-circle" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" alt="..." />
+                                    <img className="rounded-circle" src="./aboutUs-Aasma.jpg" alt="..." />
                                 </div>
                                 <div class="card-body text-center">
                                     <h5 class="card-title">Aasma</h5>
@@ -88,7 +88,7 @@ class AboutUs extends React.Component {
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
                                 <div class="card-body text-center">
-                                    <img className="rounded-circle" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" alt="..." />
+                                    <img className="rounded-circle" src="./aboutUs-Abed.jpg" alt="..." />
                                 </div>
                                 <div class="card-body text-center">
                                     <h5 class="card-title">Abed</h5>
@@ -98,8 +98,8 @@ class AboutUs extends React.Component {
                     </div>
                     <div className="paragraph" style={{paddingLeft: "100px", paddingRight: "100px"}}>
                         PerFlow is made up of a team of students in Software Development Academy 6, a coding program run by
-                        <a href="https://www.novarepotential.com/" target="_blank"> Novare Potential</a> in conjunction with
-                        <a href="https://www.kth.se/en" target="_blank"> KTH</a>. During the course, we learn Java, git, JavaScript,
+                        <a href="https://www.novarepotential.com/" target="blank"> Novare Potential</a> in conjunction with
+                        <a href="https://www.kth.se/en" target="blank"> KTH</a>. During the course, we learn Java, git, JavaScript,
                          C#, SQL, and how to use a variety of frameworks and tools like Hibernate, Spring, Gradle, and more. During the last
                           three weeks of the program, we are assigned to random groups and tasked to build an application together. PerFlow
                            is the result of our group's efforts. We are deeply grateful to Novare Potential, KTH, and each other for this
