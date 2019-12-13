54ede18c96f246d37a2b71801951b09f6c203f2c
jshortz <jessica.shortz@gmail.com>
2019-12-13 13:13:04 +0100

    Updated Slider images to be only graphs. Changed CSS to make background of graphs white

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -10,14 +10,8 @@ export default class Slider extends Component {
 
     this.state = {
         images: [
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/aurora.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/mountains.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/redsky.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/sandy-shores.jpg",
-            "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/tree-of-life.jpg"
+            "./scurve1.png",
+            "./scurve2.png"
         ],
         currentIndex: 0,
         translateValue: 0
