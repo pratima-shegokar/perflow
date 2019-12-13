4bab87bfa5db5cdcce02503916f5bf2b92ea3667
Melany <melanycristyg@gmail.com>
2019-12-13 13:26:23 +0100

    fixing class to className

--- frontend/src/components/about/AboutUs.js
+++ frontend/src/components/about/AboutUs.js
@@ -10,34 +10,34 @@ class AboutUs extends React.Component {
                     <div className="row justify-content-center">
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
+                                <div className="card-body text-center">
                                     <a href="https://www.linkedin.com/in/jessica-shortz-02572831/" target="blank" style={{ textDecoration: 'none' }}>
                                         <img className="rounded-circle" src="./aboutUs-Jessica.jpg" alt="..." />
                                     </a>
                                 </div>
                                 <div class="card-body text-center">
-                                    <h5 class="card-title">Jessica</h5>
+                                    <h5 className="card-title">Jessica</h5>
 
                                 </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
+                                <div className="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Milad.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Milad</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Milad</h5>
                                 </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
+                                <div className="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Tarek.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Tarek</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Tarek</h5>
                                 </div>
                             </div>
                         </div>
@@ -48,28 +48,28 @@ class AboutUs extends React.Component {
                                 <div class="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Melany.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Melany</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Melany</h5>
                                 </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
+                                <div className="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Pratima.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Pratima</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Pratima</h5>
                                 </div>
                             </div>
                         </div>
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
+                                <div className="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Sidhi.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Sidhi</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Sidhi</h5>
                                 </div>
                             </div>
                         </div>
@@ -77,11 +77,11 @@ class AboutUs extends React.Component {
                     <div className="row justify-content-center">
                         <div className="col-xs-12 col-md-4">
                             <div className="card mt-5 mr-5 ml-5">
-                                <div class="card-body text-center">
-                                    <img className="rounded-circle" src="./aboutUs-Aasma.jpg" alt="..." />
+                                <div className="card-body text-center">
+                                    <img className="rounded-circle" src="./aboutUs-Asmaa.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Aasma</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Asmaa</h5>
                                 </div>
                             </div>
                         </div>
@@ -90,13 +90,13 @@ class AboutUs extends React.Component {
                                 <div class="card-body text-center">
                                     <img className="rounded-circle" src="./aboutUs-Abed.jpg" alt="..." />
                                 </div>
-                                <div class="card-body text-center">
-                                    <h5 class="card-title">Abed</h5>
+                                <div className="card-body text-center">
+                                    <h5 className="card-title">Abed</h5>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="paragraph" style={{paddingLeft: "100px", paddingRight: "100px"}}>
+                    <div className="paragraph" style={{ paddingLeft: "100px", paddingRight: "100px" }}>
                         PerFlow is made up of a team of students in Software Development Academy 6, a coding program run by
                         <a href="https://www.novarepotential.com/" target="blank"> Novare Potential</a> in conjunction with
                         <a href="https://www.kth.se/en" target="blank"> KTH</a>. During the course, we learn Java, git, JavaScript,
