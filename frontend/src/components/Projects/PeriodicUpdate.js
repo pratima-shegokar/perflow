bbe91a9168593a7f4830b27b2b1a7e69e2b76330
Abdul Hameed <abdnaqip1980@gmail.com>
2019-12-14 18:30:40 +0100

    created PeriodicUpdate page which receives the project object from ProjPanel, then fetch the project records through the Api from backend database, then display them in the material-table component, once the row updated the database updated immediately.

---
+++ frontend/src/components/Projects/PeriodicUpdate.js
@@ -0,0 +1,100 @@
+import React from "react";
+import MaterialTable from "material-table";
+import RecordsApi from "../../api/RecordsApi";
+
+class PeriodicUpdate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      evs: [],
+      records: [],
+      columns: [
+        { title: "Month", field: "monthInterval" },
+        { title: "Planned Value", field: "formattedPv", type: "numeric" },
+        { title: "Earned Value", field: "ev", type: "numeric" },
+        { title: "Actual Cost", field: "ac", type: "numeric" }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    RecordsApi.getByProjectId(this.props.location.state.project.projectId)
+      .then(({ data }) => this.setState({ records: data }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    let decimalFormatter = new Intl.NumberFormat("en-IN", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
+
+    const recs = this.state.records.map(record => {
+      record.monthInterval = "Month-" + record.interval;
+      record.formattedPv = decimalFormatter.format(record.pv);
+      return record;
+    });
+    //this.state.records.interval = recs;
+    console.log("recs = " + recs);
+
+    return (
+      <React.Fragment>
+        <div align="center">
+          <MaterialTable
+            title="Periodic Data "
+            data={recs}
+            columns={this.state.columns}
+            options={{
+              rowStyle: {
+                backgroundColor: "#EEE"
+              }
+            }}
+            editable={{
+              onRowAdd: newData =>
+                new Promise(resolve => {
+                  setTimeout(() => {
+                    resolve();
+                    /*this.setState(prevState => {
+                                        const data = [...prevState.data];
+                                        data.push(newData);
+                                        return { ...prevState, data };
+                                    });*/
+                  }, 300);
+                }),
+              onRowUpdate: newData =>
+                new Promise(resolve => {
+                  newData.updated = true;
+                  RecordsApi.updateRecord(newData);
+                  const records = this.state.records.map(record => {
+                    if (record.recordId == newData.recordId) {
+                      return newData;
+                    } else {
+                      return record;
+                    }
+                  });
+                  this.setState({ records }, () => resolve());
+                }),
+              onRowDelete: oldData =>
+                new Promise(resolve => {
+                  setTimeout(() => {
+                    resolve();
+                    /*this.setState(prevState => {
+                                        const data = [...prevState.data];
+                                        data.splice(data.indexOf(oldData), 1);
+                                        return { ...prevState, data };
+                                    });*/
+                  }, 300);
+                })
+            }}
+            style={{
+              width: "60%"
+            }}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PeriodicUpdate;
